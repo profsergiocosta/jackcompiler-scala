803cae6bece2fbd74c7e1daf13ad0b4aa5792ac2
sergio costa <prof.sergio.costa@gmail.com>
2019-05-13 21:19:28 -0300

    iniciando tabela de simbolo

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -4,6 +4,40 @@ package JackCompiler
 
 import java.io._
 
+
+case class Symbol (val t:String, val k:String, val i:Int)
+
+class SymbolTable {
+    var classScope = scala.collection.mutable.Map[String, Symbol]()
+    var localScope = scala.collection.mutable.Map[String, Symbol]()
+
+    var count = scala.collection.mutable.Map[String, Int]()
+    count += ("FIELD" -> 0, "STATIC" -> 0, "ARG" -> 0, "VAR" -> 0)
+
+
+    def define(name:String, sType: String, kind:String)  {
+        if (kind ==  "STATIC" || kind == "FIELD") {
+            classScope  += (name -> Symbol(name, kind, count(kind)))
+        } else {
+            localScope  += (name -> Symbol(name, kind, count(kind)))
+        }
+        count(kind) = count(kind)+1
+    }
+
+    def varCount(kind:String) = count(kind)
+    
+    def kindOf(name:String) : String =  {
+        if (localScope contains name) {
+            return localScope(name).k
+        } else {
+            return classScope(name).k
+        }
+        return "NotFound"
+    }
+
+
+}
+
 class JackCompiler {
   
   val comp = new CompilationEngine ("Main.jack")
