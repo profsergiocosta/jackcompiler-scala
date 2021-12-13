f20ad7a79a05bd6968d4470eab69bff47d64c910
Sergio Costa <sergio.costa@ufma.br>
2021-12-13 18:40:22 -0300

    clean visitor

--- src/main/scala/Ast.scala
+++ src/main/scala/Ast.scala
@@ -3,33 +3,23 @@ package jackcompiler.ast
 abstract class Visitor {
     def visitLetStatement (v: LetStatement) : Unit
     def visitExpression (v: Expression) : Unit
-    def visitVariable (v: Variable) : Unit
-    def visitIdentifier (v: Identifier) : Unit
-    def visitLiteraInteger (v: IntegerLiteral) : Unit
 }
 
 abstract class Node {
     def accept(v: Visitor) :Unit
 }
 
-abstract class Expression extends Node
+abstract class Expression extends Node {
+     def accept (v: Visitor) = {
+        return v.visitExpression(this)
+    }
+}
 abstract class Statement extends Node
 abstract class Identifier extends Expression
-case class Variable (val varName:String) extends Identifier {
-    def accept (v: Visitor) = {
-        return v.visitVariable(this)
-    }
-}
-case class IndexVariable (val varName: String, exp: Expression) extends Identifier{
-    def accept (v: Visitor) = {
-        
-    }
-}
-case class IntegerLiteral (val value:Int) extends Expression {
-    def accept (v: Visitor) = {
-        return v.visitLiteraInteger(this)
-    }
-}
+case class Variable (val varName:String) extends Identifier 
+case class IndexVariable (val varName: String, exp: Expression) 
+case class IntegerLiteral (val value:Int) extends Expression 
+
 case class LetStatement (val id:Identifier, val exp:Expression) extends Statement{
     def accept (v: Visitor) = {
         return v.visitLetStatement(this)
--- src/main/scala/AstPrinter.scala
+++ src/main/scala/AstPrinter.scala
@@ -5,29 +5,21 @@ import jackcompiler.ast.*
 class AstPrinter extends Visitor {
     def visitLetStatement (v: LetStatement) = {
         print ("(def ")
-        visitIdentifier (v.id)
+        visitExpression (v.id)
         visitExpression(v.exp)
         print (") ")
     }
     def visitExpression (v: Expression) = {
         v match {
             case IntegerLiteral (v) => print (v)
-        }
-    }
-
-    def visitIdentifier (v: Identifier) = {
-        v match {
             case Variable (varname) => {
                 print (" ")
                 print(varname)
-                 print (" ")
+                print (" ")
             }
+
+            case _ => {}
         }
     }
-    def visitVariable (v: Variable) = {
-        println(v.varName)
-    }
-    def visitLiteraInteger (v: IntegerLiteral) = {
-        println(v.value)
-    }
+
 }
